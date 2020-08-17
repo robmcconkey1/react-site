7d6e45427322e9c110adf2e24cf9154af62404eb
Rob McConkey <robmcconkey@Robs-MacBook-Pro-3.local>
2020-08-17 20:52:34 +0100

    Updates

---
+++ precache-manifest.1ac8e96da14eebccffe2b93309370fca.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1e43686ac33eb22d6ac293d51c51af",
+    "url": "/react-site/index.html"
+  },
+  {
+    "revision": "037a938c2054f89b029a",
+    "url": "/react-site/static/css/2.06c478a6.chunk.css"
+  },
+  {
+    "revision": "73ecf62fad12385ce167",
+    "url": "/react-site/static/css/main.9332eed1.chunk.css"
+  },
+  {
+    "revision": "037a938c2054f89b029a",
+    "url": "/react-site/static/js/2.03a34196.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-site/static/js/2.03a34196.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73ecf62fad12385ce167",
+    "url": "/react-site/static/js/main.b39db8c4.chunk.js"
+  },
+  {
+    "revision": "4938e7870f22f9feecae",
+    "url": "/react-site/static/js/runtime-main.4eed6354.js"
+  },
+  {
+    "revision": "a5d55c91f74e4219afed3672f77ffc07",
+    "url": "/react-site/static/media/1.a5d55c91.jpg"
+  },
+  {
+    "revision": "70aa4614f5ec3e01c9ba156ddfa32ace",
+    "url": "/react-site/static/media/resize2.1.70aa4614.jpg"
+  },
+  {
+    "revision": "ca2e1cec560cac24324d5c87b6bbce05",
+    "url": "/react-site/static/media/travel4-4.ca2e1cec.jpg"
+  },
+  {
+    "revision": "373c1990a439513c5738116250355427",
+    "url": "/react-site/static/media/travel61.373c1990.jpg"
+  }
+]);

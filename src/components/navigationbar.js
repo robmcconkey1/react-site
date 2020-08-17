fdbe204c9bbd2caf7e7026bac1b6e213f1c4257c
Rob McConkey <robmcconkey@Robs-MacBook-Pro-3.local>
2020-08-17 21:45:35 +0100

    commit7

--- src/components/navigationbar.js
+++ src/components/navigationbar.js
@@ -14,16 +14,16 @@ export const NavigationBar = () => (
   <Navbar.Toggle aria-controls="basic-navbar-nav"/>
   <Navbar.Collapse id="basic-navbar-nav">
   <Nav className= "ml-auto">
-  <Nav.Item><Nav.Link href="/"> Home </Nav.Link> </Nav.Item>
-<Nav.Item><Nav.Link href="/street"> Street </Nav.Link> </Nav.Item>
-<Nav.Item><Nav.Link href="/travel"> Travel </Nav.Link> </Nav.Item>
-<Nav.Item><Nav.Link href="/weddings"> Weddings </Nav.Link> </Nav.Item>
+  <Nav.Item><Nav.Link href="/react-site/"> Home </Nav.Link> </Nav.Item>
+<Nav.Item><Nav.Link href="/react-site/street"> Street </Nav.Link> </Nav.Item>
+<Nav.Item><Nav.Link href="/react-site/travel"> Travel </Nav.Link> </Nav.Item>
+<Nav.Item><Nav.Link href="/react-site/weddings"> Weddings </Nav.Link> </Nav.Item>
 <Nav.Item><Nav.Link href="https://www.instagram.com/rob_mcconkey/?hl=en" target="_blank"> Instagram </Nav.Link> </Nav.Item>
 <NavDropdown title="Blog" id="basic-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Canterbury</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">London Mews</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.3">London Underground</NavDropdown.Item>
-      
+
       </NavDropdown>
 
 </Nav>

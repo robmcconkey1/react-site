69dc138b0baacd3f526e0750511cd9718f2d63ed
Rob McConkey <robmcconkey@Robs-MacBook-Pro-3.local>
2020-08-17 20:42:54 +0100

    commit2

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import * as serviceWorker from './serviceWorker';
+import  { HashRouter } from 'react-router-dom';
 
 
 
 ReactDOM.render(
   <React.StrictMode>
+  <HashRouter>
     <App />
-
+</HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );

84e7221ec7f08cd218c093a1ad7e0f01aa2a3e9f
Rob McConkey <robmcconkey@Robs-MacBook-Pro-3.local>
2020-08-19 22:31:21 +0100

    Updates

---
+++ precache-manifest.28f7e746529931827ae9533ef3448352.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f4c91f1f815f7ee1e2cde6141c41cc5",
+    "url": "/react-site/index.html"
+  },
+  {
+    "revision": "71f9a7a4e99abbdd5441",
+    "url": "/react-site/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "220bf51d5c41442dbe3b",
+    "url": "/react-site/static/css/main.f42491f4.chunk.css"
+  },
+  {
+    "revision": "71f9a7a4e99abbdd5441",
+    "url": "/react-site/static/js/2.7ae6f8eb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-site/static/js/2.7ae6f8eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "220bf51d5c41442dbe3b",
+    "url": "/react-site/static/js/main.1b01869b.chunk.js"
+  },
+  {
+    "revision": "4938e7870f22f9feecae",
+    "url": "/react-site/static/js/runtime-main.4eed6354.js"
+  },
+  {
+    "revision": "c63351e0e443e3bdd6dcbcd9c2af41cb",
+    "url": "/react-site/static/media/resize1.c63351e0.jpg"
+  },
+  {
+    "revision": "090f4594454ea279de774dcdb2a89512",
+    "url": "/react-site/static/media/resize2.2.090f4594.jpg"
+  },
+  {
+    "revision": "83ab997cae4a0ed476c301b51d96ff74",
+    "url": "/react-site/static/media/resize3.1.83ab997c.jpg"
+  },
+  {
+    "revision": "3672edfe53e56086812c5ec6339d5855",
+    "url": "/react-site/static/media/websitelondon3.3672edfe.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a18958e59808d9197698730229f4dc5f.js"
+  "/react-site/precache-manifest.28f7e746529931827ae9533ef3448352.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-site/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
